504349764edd97d136b9775a98ace42a5dd3c066
Christian@dev <kamgaingchristian1989@gmail.com>
2023-01-19 11:50:07 +0100

    Initial commit

--- src/customHooks/importScript.jsx
+++ src/customHooks/importScript.jsx
@@ -1,24 +1,24 @@
 import { useEffect } from 'react';
 
-const ImportScript = resourceUrl=> {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = resourceUrl;
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, [resourceUrl]);
-};
+// const ImportScript = resourceUrl=> {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = resourceUrl;
+//     script.async = true;
+//     document.body.appendChild(script);
+//     return () => {
+//       document.body.removeChild(script);
+//     }
+//   }, [resourceUrl]);
+// };
 
 const ImportScriptJS = resourceUrl=> {
   let firstLoading = true;
 
   useEffect(() => {
-    'use strict';
+    //'use strict';
 
-    if( firstLoading == false )
+    if( firstLoading === false )
       return;
       
     // modal variables
