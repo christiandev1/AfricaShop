3139d91bb81ad1b1abd200a0343e73a1b5d93ef9
Christian@dev <kamgaingchristian1989@gmail.com>
2023-01-20 11:20:06 +0100

    Initiaal commit

--- src/shared/constant.tsx
+++ src/shared/constant.tsx
@@ -3,36 +3,8 @@ const logo = require('../assets/images/logo/logo.svg');
 
 
 export const AppSettings = {
-    "AppName" : "Made In Africa",
+    "AppName" : "Made In Africa Shops",
     "AppLogo" : {logo},
-    "AltLogo" : "Made In Africa's logo"
+    "AltLogo" : "Made In Africa Shops's logo"
 };
-
-export const FooterItemArray = [
-
-    {
-        text:"Accueil",
-        url: "#Accueil",
-    },
-    {
-        text:"Qui sommes-nous ?",
-        url: "#QuiSommesNous",
-    },
-    {
-        text:"Comment ça marche ?",
-        url: "#CommentCaMarche",
-    },
-    {
-        text:"Contact",
-        url: "#Contact",
-    },
-    {
-        text:"Mentions légales",
-        url: "#MentionsLegales",
-    },
-    {
-        text:"Confidentialité",
-        url: "#Confidentialite",
-    },
-]
 
